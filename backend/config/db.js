5230654d0f89bb090c01f4ba340262818edd6f68
akanoao <dakshgakhar18@gmail.com>
2025-03-29 01:48:26 +0530

    change mongo uri

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-mongoose.connect("mongodb://localhost/appd");
+mongoose.connect(process.env.MONGO_URI);
 const db = mongoose.connection;
 db.on("error", (err) => console.error(err));
 db.once("open", () => console.log("Connected to database"));
